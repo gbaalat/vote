80499e03752fb04cec7f9723f609498ef56d4ad8
nicolas <gbaalat@gmail.com>
2022-04-19 11:08:35 +0200

    webhook2

--- src/vote/general/endpoints.py
+++ src/vote/general/endpoints.py
@@ -1,12 +1,12 @@
 from flask import Blueprint
 import git
 
-auth_bp = Blueprint(
+gen_bp = Blueprint(
     "gen_bp", __name__, template_folder="templates", static_folder="static"
 )
 
 
-@auth_bp.route("/git_update", methods=["POST"])
+@gen_bp.route("/git_update", methods=["POST"])
 def git_update():
     repo = git.Repo("./vote")
     origin = repo.remotes.origin
