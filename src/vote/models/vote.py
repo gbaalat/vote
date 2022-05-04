62e2d1a124cec646c6dc4a0334ac33cb0700dbf7
Côme <come.mainguy@gmail.com>
2022-05-04 14:10:21 +0200

    ajout d'un id au modèle vote

--- src/vote/models/vote.py
+++ src/vote/models/vote.py
@@ -7,7 +7,8 @@ from sqlalchemy import ForeignKeyConstraint
 
 class Vote(db.Model):
     """Modèle vote pour la base de données"""
-
+    
+    id = db.Column(db.Integer, primary_key=True, autoincrement=True)
     id_user = db.Column(db.Integer, db.ForeignKey("utilisateur.id"), primary_key=True)
     id_candidat = db.Column(db.Integer, db.ForeignKey("candidat.id"), nullable=False)
     categorie = db.Column(db.Integer, db.ForeignKey("categorie.id"), primary_key=True)
