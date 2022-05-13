53700d559e0a0c62868372c04a492119dbc16dfe
ilona <ilolab15@gmail.com>
2022-05-13 09:39:51 +0200

    en cours connexion

--- src/vote/auth/endpoints.py
+++ src/vote/auth/endpoints.py
@@ -1,4 +1,5 @@
 from flask import Blueprint, render_template, request
+from vote.auth.business import validation
 
 auth_bp = Blueprint('auth_bp', __name__,
     template_folder='templates',
@@ -19,8 +20,12 @@ def creerCompte():
     """Page dont le lien est contenu dans le mail d'inscription."""
     pass
 
-@auth_bp.route('/connexion')
+@auth_bp.route('/connexion', methods=["GET", "POST"])
 def connexion():
+    if request.method == "POST":
+        email = request.form['email']
+        mdp = request.form['mdp']
+        validation(email, mdp)
     return render_template("auth_connexion.html")
 
 @auth_bp.route('/motDePasseOublie')
