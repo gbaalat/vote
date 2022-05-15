099fce6bef6f36a0deefc97054ce73dd60e25f88
ilona <ilolab15@gmail.com>
2022-05-15 23:07:50 +0200

    login - fonctionne mais pas à partir de business.py, donc le code est aussi dans endpoints.py

--- src/vote/auth/business.py
+++ src/vote/auth/business.py
@@ -1,22 +1,14 @@
-from flask import flash, url_for, redirect, session
+from flask import flash, redirect, session, url_for
 from vote.models.utilisateur import Utilisateur
 
 def validation(email, mdp):
-    if not(email and mdp) and len(mdp)<8:
-        flash("Entrez des valeurs valides")
-        return redirect(url_for('auth_bp.connexion'))
-    else:
-        email = email.strip()   #enlève les espaces superflus
-        mdp = mdp.strip()
-    
     u = Utilisateur.query.filter_by(email=email).first()
-
-    if u.verify_email(email) and u.verify_mdp(email, mdp):
-        session['id'] = u.id
-        return redirect(url_for("nav_bp.nav"))
-    else:
+    if u is None or not(u.verify_email(email) and u.verify_mdp(email, mdp)):
         flash("Email ou mot de passe incorrect, veuillez réessayer")
         return redirect(url_for('auth_bp.connexion'))
+    else:
+        session['id'] = u.id
+        return redirect(url_for("nav_bp.nav"))
 
 def enregistrerMdp(publicKey, mdp):
     pass
--- src/vote/auth/endpoints.py
+++ src/vote/auth/endpoints.py
@@ -1,5 +1,6 @@
-from flask import Blueprint, render_template, request
+from flask import Blueprint, render_template, request, session, flash, url_for, redirect
 from vote.auth.business import validation
+from vote.models.utilisateur import Utilisateur
 
 auth_bp = Blueprint('auth_bp', __name__,
     template_folder='templates',
@@ -25,7 +26,16 @@ def connexion():
     if request.method == "POST":
         email = request.form['email']
         mdp = request.form['mdp']
-        validation(email, mdp)
+        email, mdp = email.strip(), mdp.strip()   #enlève les espaces superflus
+        
+        u = Utilisateur.query.filter_by(email=email).first()
+
+        if u is None or not(u.verify_email(email) and u.verify_mdp(email, mdp)):
+            flash("Email ou mot de passe incorrect, veuillez réessayer")
+            return redirect(url_for('auth_bp.connexion'))
+        else:
+            session['id'] = u.id
+            return redirect(url_for("nav_bp.nav"))
     return render_template("auth_connexion.html")
 
 @auth_bp.route('/motDePasseOublie')
