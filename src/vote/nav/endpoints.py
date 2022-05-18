8c0c30ec547d7b20c6f630455e79a06ceecf23f5
Côme <come.mainguy@gmail.com>
2022-05-18 21:08:11 +0200

    Ajout d'un message montrant si l'on a déjà voté dans une catégorie

--- src/vote/nav/endpoints.py
+++ src/vote/nav/endpoints.py
@@ -1,5 +1,6 @@
 from flask import Blueprint, render_template
 from vote.models.categorie import Categorie
+from vote.models.vote import Vote
 
 nav_bp = Blueprint(
     "nav_bp",
@@ -13,4 +14,4 @@ nav_bp = Blueprint(
 @nav_bp.route("/")
 def nav():
     categories = Categorie.query.all()
-    return render_template("index.html", categories=categories)
+    return render_template("index.html", categories=categories, Vote = Vote)
