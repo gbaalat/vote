2e7633649344c26afe0bf123662dd9adc30f2de0
labat <gbaalat@gmail.com>
2022-05-19 14:19:28 +0200

    ajout du champ Categorie.ouvert et gestion des ex-aequo pour le podium

--- src/vote/models/categorie.py
+++ src/vote/models/categorie.py
@@ -11,12 +11,13 @@ class Categorie(db.Model):
     id = db.Column(db.Integer, primary_key=True, autoincrement=True)
     niveau = db.Column(db.String(1), nullable=False)
     genre = db.Column(db.String(1), nullable=False)
-    ouvert = True
+    ouvert = db.Column(db.Boolean, default=False)
 
     def __repr__(self):
         return f"<Categorie id={self.id} niveau={self.niveau}, genre={self.genre}>"
-    def ouverture(self):
-        if self.ouvert == True:
+    
+    def swap_ouverture(self):
+        if self.ouvert:
             self.ouvert = False
         else:
             self.ouvert = True
